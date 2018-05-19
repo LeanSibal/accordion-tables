32931106cf24faa22fee198b8c704e68816b34e3
Lean Sibal <lsibal@apdgroup.com>
2018-05-19 19:29:15 +0800

    Dynamic table headers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 jQuery(function($){
 	$('#accordion_tables .accordion_container').accordion({
 		active: false,
-		collapsible: true
+		collapsible: true,
+		beforeActivate: function( event, ui ) {
+			$('#accordion_tables .fa-minus-circle').each(function(){
+				$(this).removeClass('fa-minus-circle').addClass('fa-plus-circle');
+			});
+			$(ui.newHeader.context).find('i.fa-plus-circle').removeClass('fa-plus-circle').addClass('fa-minus-circle');
+		}
 
 	});
 });
--- plugin.php
+++ plugin.php
@@ -14,7 +14,12 @@ class AccordionTables {
 	public function __construct() {
 		add_action('wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
 		add_shortcode('accordion_tables', [ $this, 'accordion_tables_shortcode' ] );
-		add_action('init', [ $this, 'register_post_type' ] );
+		add_action( 'init', [ $this, 'register_taxonomy' ] );
+		add_action( 'row_headers_add_form_fields', [ $this, 'row_headers_add_form_fields' ] );
+		add_action( 'row_headers_edit_form_fields', [ $this, 'row_headers_add_form_fields' ] );
+		add_action( 'create_row_headers', [ $this, 'save_row_headers' ] );
+		add_action( 'edit_row_headers', [ $this, 'save_row_headers' ] );
+		add_action( 'init', [ $this, 'register_post_type' ] );
 		add_action( 'add_meta_boxes', [ $this, 'accordion_labels_meta_box' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'admin_enqueue_scripts' ] );
 		add_action( 'save_post', [ $this, 'save_accordion_labels_meta_box_data' ] );
@@ -35,12 +40,18 @@ class AccordionTables {
 		if( ! wp_verify_nonce( $_POST['accordion_tables_nonce'], 'accordion_tables_nonce' ) ) return $post_id;
 		if( empty( $_POST['post_type'] ) || $_POST['post_type'] !== 'accordion_tables' ) return $post_id;
 		if( defined( 'DOING_AUTOSAVE') && DOING_AUTOSAVE ) return $post_id;
-		$properties = ['','','',''];
-		for( $i = 0; $i < 5; $i++ ) {
-			if( !empty( $_POST['properties'][$i]['type'] ) && $_POST['properties'][$i]['type'] == 'custom_value' ) {
-				$properties[$i] = $_POST['properties'][$i]['text'];
-			} else if( !empty( $_POST['properties'][$i]['type'] ) ) {
-				$properties[$i] = $_POST['properties'][$i]['type'];
+		$row_headers = get_terms( 'row_headers', [
+			'hide_empty' => false,
+			'orderby' => 'meta_value_num',
+			'order' => 'ASC',
+			'meta_key' => 'tax-order'
+		]);
+		$properties = [];
+		foreach( $row_headers as $row_header ) {
+			if( !empty( $_POST['properties'][$row_header->slug]['type'] ) && $_POST['properties'][$row_header->slug]['type'] == 'custom_value' ) {
+				$properties[$row_header->slug] = $_POST['properties'][$row_header->slug]['text'];
+			} else if( !empty( $_POST['properties'][$row_header->slug]['type'] ) ) {
+				$properties[$row_header->slug] = $_POST['properties'][$row_header->slug]['type'];
 			}
 		}
 		$order = !empty( $_POST['list_order'] ) ? $_POST['list_order'] : 0;
@@ -55,6 +66,12 @@ class AccordionTables {
 		wp_nonce_field( 'accordion_tables_nonce', 'accordion_tables_nonce' );
 		$properties = get_post_meta( $post->ID, '_accordion_labels', true );
 		$order = get_post_meta( $post->ID, '_list_order', true );
+		$row_headers = get_terms( 'row_headers', [
+			'hide_empty' => false,
+			'orderby' => 'meta_value_num',
+			'order' => 'ASC',
+			'meta_key' => 'tax-order'
+		]);
 ?>
 <div id="accordion_tables_meta_box">
 
@@ -63,87 +80,29 @@ class AccordionTables {
 		<input type="number" name="list_order" min="0" value="<?php echo !empty( $order ) ? $order : '0'; ?>">
 	</label>
 
-	<h3>Dynamics 365 Business Central Essen al Basic</h3>
-	<label>
-		<input type="radio" name="properties[0][type]" value="" <?php echo empty( $properties[0] ) ? 'checked' : ''; ?>/> Empty
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[0][type]" value="check" <?php echo ( !empty( $properties[0] ) && $properties[0] == 'check' ) ? 'checked' : ''; ?>/>
-		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/check-green.svg' ); ?>"/>
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[0][type]" value="phone" <?php echo ( !empty( $properties[0] ) && $properties[0] == 'phone' ) ? 'checked' : ''; ?>/>
-		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/phone-green.svg' ); ?>"/>
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[0][type]" value="custom_value" <?php echo ( !empty( $properties[0] ) && $properties[0] !== 'phone' && $properties[0] !== 'check' ) ? 'checked' : ''; ?>/>
-		<input class="custom_value" type="text" name="properties[0][text]" placeholder="Custom Value" value="<?php echo ( !empty( $properties[0] ) && $properties[0] !== 'phone' && $properties[0] !== 'check' ) ? $properties[0] : ''; ?>"/>
-	</label>
+<?php foreach( $row_headers as $row_header ): ?>
 
-	<h3>Dynamics 365 Business Central Essen al</h3>
+	<h3><?php echo $row_header->name; ?></h3>
+	<small><?php echo $row_header->description; ?></small><br/>
 	<label>
-		<input type="radio" name="properties[1][type]" value="" <?php echo empty( $properties[1] ) ? 'checked' : ''; ?>/> Empty
+		<input type="radio" name="properties[<?php echo $row_header->slug; ?>][type]" value="" <?php echo empty( $properties[$row_header->slug] ) ? 'checked' : ''; ?>/> Empty
 	</label>
 	<br/>
 	<label>
-		<input type="radio" name="properties[1][type]" value="check" <?php echo ( !empty( $properties[1] ) && $properties[1] == 'check' ) ? 'checked' : ''; ?>/>
+		<input type="radio" name="properties[<?php echo $row_header->slug; ?>][type]" value="check" <?php echo ( !empty( $properties[$row_header->slug] ) && $properties[$row_header->slug] == 'check' ) ? 'checked' : ''; ?>/>
 		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/check-green.svg' ); ?>"/>
 	</label>
 	<br/>
 	<label>
-		<input type="radio" name="properties[1][type]" value="phone" <?php echo ( !empty( $properties[1] ) && $properties[1] == 'phone' ) ? 'checked' : ''; ?>/>
+		<input type="radio" name="properties[<?php echo $row_header->slug; ?>][type]" value="phone" <?php echo ( !empty( $properties[$row_header->slug] ) && $properties[$row_header->slug] == 'phone' ) ? 'checked' : ''; ?>/>
 		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/phone-green.svg' ); ?>"/>
 	</label>
 	<br/>
 	<label>
-		<input type="radio" name="properties[1][type]" value="custom_value" <?php echo ( !empty( $properties[1] ) && $properties[1] !== 'phone' && $properties[1] !== 'check' ) ? 'checked' : ''; ?>/>
-		<input class="custom_value" type="text" name="properties[1][text]" placeholder="Custom Value" value="<?php echo ( !empty( $properties[1] ) && $properties[1] !== 'phone' && $properties[1] !== 'check' ) ? $properties[1] : ''; ?>"/>
+		<input type="radio" name="properties[<?php echo $row_header->slug; ?>][type]" value="custom_value" <?php echo ( !empty( $properties[$row_header->slug] ) && $properties[$row_header->slug] !== 'phone' && $properties[$row_header->slug] !== 'check' ) ? 'checked' : ''; ?>/>
+		<input class="custom_value" type="text" name="properties[<?php echo $row_header->slug; ?>][text]" placeholder="Custom Value" value="<?php echo ( !empty( $properties[$row_header->slug] ) && $properties[$row_header->slug] !== 'phone' && $properties[$row_header->slug] !== 'check' ) ? $properties[$row_header->slug] : ''; ?>"/>
 	</label>
-
-	<h3>Dynamics 365 Byg & Anlæg</h3>
-	<small>Op  l 10 administra ve brugere</small><br/>
-	<label>
-		<input type="radio" name="properties[2][type]" value="" <?php echo empty( $properties[2] ) ? 'checked' : ''; ?>/> Empty
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[2][type]" value="check" <?php echo ( !empty( $properties[2] ) && $properties[2] == 'check' ) ? 'checked' : ''; ?>/>
-		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/check-green.svg' ); ?>"/>
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[2][type]" value="phone" <?php echo ( !empty( $properties[2] ) && $properties[2] == 'phone' ) ? 'checked' : ''; ?>/>
-		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/phone-green.svg' ); ?>"/>
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[2][type]" value="custom_value" <?php echo ( !empty( $properties[2] ) && $properties[2] !== 'phone' && $properties[2] !== 'check' ) ? 'checked' : ''; ?>/>
-		<input class="custom_value" type="text" name="properties[2][text]" placeholder="Custom Value" value="<?php echo ( !empty( $properties[2] ) && $properties[2] !== 'phone' && $properties[2] !== 'check' ) ? $properties[2] : ''?>"/>
-	</label>
-
-	<h3>Dynamics NAV Byg & Anlæg</h3>
-	<small>+ 10 administra ve brugere</small><br/>
-	<label>
-		<input type="radio" name="properties[3][type]" value="" <?php echo empty( $properties[3] ) ? 'checked' : ''; ?>/> Empty
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[3][type]" value="check" <?php echo ( !empty( $properties[3] ) && $properties[3] == 'check' ) ? 'checked' : ''; ?>/>
-		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/check-green.svg' ); ?>"/>
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[3][type]" value="phone" <?php echo ( !empty( $properties[3] ) && $properties[3] == 'phone' ) ? 'checked' : ''; ?>/>
-		<img class="accordion_table_icon" src="<?php echo plugins_url( '/accordion-tables/images/phone-green.svg' ); ?>"/>
-	</label>
-	<br/>
-	<label>
-		<input type="radio" name="properties[3][type]" value="custom_value" <?php echo ( !empty( $properties[3] ) && $properties[3] !== 'phone' && $properties[3] !== 'check' ) ? 'checked' : ''; ?>/>
-		<input class="custom_value" type="text" name="properties[3][text]" placeholder="Custom Value" value="<?php echo ( !empty( $properties[3] ) && $properties[3] !== 'phone' && $properties[3] !== 'check' ) ? $properties[3] : ''; ?>"/>
-	</label>
+<?php endforeach; ?>
 
 </div>
 <?php
@@ -163,6 +122,65 @@ class AccordionTables {
 			[],
 			$this->version
 		);
+		wp_register_style(
+			'fontawesome',
+			'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
+			[],
+			'5.0.13'
+		);
+
+	}
+
+	public function register_taxonomy() {
+		$labels = array(
+			'name'              => _x( 'Row Headers', 'taxonomy general name', 'accordion_tables' ),
+			'singular_name'     => _x( 'Row Header', 'taxonomy singular name', 'accordion_tables' ),
+			'search_items'      => __( 'Search Row Headers', 'accordion_tables' ),
+			'all_items'         => __( 'All Row Headers', 'accordion_tables' ),
+			'parent_item'       => __( 'Parent Row Header', 'accordion_tables' ),
+			'parent_item_colon' => __( 'Parent Row Header:', 'accordion_tables' ),
+			'edit_item'         => __( 'Edit Row Header', 'accordion_tables' ),
+			'update_item'       => __( 'Update Row Header', 'accordion_tables' ),
+			'add_new_item'      => __( 'Add New Row Header', 'accordion_tables' ),
+			'new_item_name'     => __( 'New Row Header Name', 'accordion_tables' ),
+			'menu_name'         => __( 'Row Header', 'accordion_tables' ),
+		);
+
+		$args = array(
+			'hierarchical'      => false,
+			'labels'            => $labels,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'sort'              => true,
+			'rewrite'           => array( 'slug' => 'headers' ),
+		);
+
+		register_taxonomy( 'row_headers', array( 'accordion_tables' ), $args );
+	}
+	public function row_headers_add_form_fields( $term ) {
+		$tax_order = "0";
+		if( !empty( $term->term_id ) ) {
+			$tax_order = get_term_meta( $term->term_id, 'tax-order', true );
+		}
+		?>
+ 
+	<tr class="form-field">
+		<th scope="row" valign="top">
+			<label for="tax-order">List Order</label>
+		</th>
+		<td>
+			<input name="tax-order" id="tax-order" type="number" value="<?php echo $tax_order; ?>" size="40" aria-required="true" />
+			<p class="description">Determines the order in which the row header is displayed.</p>
+		</td>
+	</tr>
+		<?php
+	}
+
+	public function save_row_headers( $term_id ) {
+		if( isset( $_POST['tax-order'] ) ) {
+			update_term_meta( $term_id, 'tax-order', $_POST['tax-order'] );
+		}
 	}
 
 	public function register_post_type() {
@@ -198,6 +216,7 @@ class AccordionTables {
 			'menu_position'      => true,
 			'menu_icon'          => 'dashicons-feedback',
 			'supports'           => array( 'title', 'editor' ),
+			'taxonomies'         => array( 'row_headers' ),
 			'register_meta_box_cb' => [ $this, 'accordion_labels_meta_box' ]
 		);
 
@@ -205,9 +224,15 @@ class AccordionTables {
 
 	}
 
+	private function floorp($val, $precision) {
+		    $mult = pow(10, $precision);
+			return floor($val * $mult) / $mult;
+	}
+
 	public function accordion_tables_shortcode() {
 		wp_enqueue_script('accordion_tables');
 		wp_enqueue_style('accordion_tables');
+		wp_enqueue_style('fontawesome');
 		$accordion_items = new WP_Query([
 			'post_type' => 'accordion_tables',
 			'order' => 'ASC',
@@ -215,6 +240,13 @@ class AccordionTables {
 			'posts_per_page' => -1,
 			'meta_key' => '_list_order'
 		]);
+		$row_headers = get_terms( 'row_headers', [
+			'hide_empty' => false,
+			'orderby' => 'meta_value_num',
+			'order' => 'ASC',
+			'meta_key' => 'tax-order'
+		]);
+		$row_width = $this->floorp( 100 / ( count( $row_headers ) + 1 ), 2 );
 		if( !$accordion_items->have_posts() ) return;
 		ob_start();
 ?>
@@ -223,77 +255,38 @@ class AccordionTables {
 		<div class="one-fifth nostyle">
 			<p>&nbsp;</p>
 		</div>
-		<div class="one-fifth">
-			<div class="spacer">
-				<h4>Dynamics 365 Business Central Essential Basic</h4>
-			</div>
-		</div>
-		<div class="one-fifth">
-			<div class="spacer">
-				<h4>Dynamics 365 Business Central Essential</h4>
-			</div>
-		</div>
-		<div class="one-fifth">
-			<div class="spacer">
-				<h4>Dynamics 365 Byg & Anlæg</h4>
-				<small>Op  l 10 administra ve brugere</small>
-			</div>
-		</div>
-		<div class="one-fifth">
-			<div class="spacer">
-				<h4>Dynamics 365 Byg & Anlæg</h4>
-				<small>+ 10 administra ve brugere</small>
-			</div>
+		<div class="four-fifth flex">
+		<?php foreach( $row_headers as $row_header ): ?>
+			<div class="flex-grow" style="width:<?php echo $row_width; ?>%">
+					<div class="spacer">
+						<h4><?php echo $row_header->name; ?></h4>
+						<span><?php echo $row_header->description; ?></span>
+					</div>
+				</div>
+		<?php endforeach; ?>
 		</div>
 	</div>
 	<div class="accordion_container">
 <?php while( $accordion_items->have_posts() ): $accordion_items->the_post(); ?>
 <?php $property = get_post_meta( get_the_ID(), '_accordion_labels', true ); ?>
 		<h3>
-				<div class="one-fifth align-right"><?php the_title(); ?></div>
-				<div class="one-fifth">
-					<?php if( empty( $property[0] ) ) : ?>
-						<p>&nbsp;</p>
-					<?php elseif( $property[0] == 'check' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/check-green.svg'); ?>"/>
-					<?php elseif( $property[0] == 'phone' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/phone-green.svg'); ?>"/>
-					<?php else: ?>
-						<p><?php echo $property[0]; ?></p>
-					<?php endif; ?>
+				<div class="one-fifth align-right">
+					<span class="accordion_title"><?php the_title(); ?></span><i class="fas fa-plus-circle"></i>
 				</div>
-				<div class="one-fifth">
-					<?php if( empty( $property[1] ) ) : ?>
-						<p>&nbsp;</p>
-					<?php elseif( $property[1] == 'check' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/check-green.svg'); ?>"/>
-					<?php elseif( $property[1] == 'phone' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/phone-green.svg'); ?>"/>
-					<?php else: ?>
-						<p><?php echo $property[1]; ?></p>
-					<?php endif; ?>
-				</div>
-				<div class="one-fifth">
-					<?php if( empty( $property[2] ) ) : ?>
-						<p>&nbsp;</p>
-					<?php elseif( $property[2] == 'check' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/check-green.svg'); ?>"/>
-					<?php elseif( $property[2] == 'phone' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/phone-green.svg'); ?>"/>
-					<?php else: ?>
-						<p><?php echo $property[2]; ?></p>
-					<?php endif; ?>
-				</div>
-				<div class="one-fifth">
-					<?php if( empty( $property[3] ) ) : ?>
-						<p>&nbsp;</p>
-					<?php elseif( $property[3] == 'check' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/check-green.svg'); ?>"/>
-					<?php elseif( $property[3] == 'phone' ) : ?>
-						<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/phone-green.svg'); ?>"/>
-					<?php else: ?>
-						<p><?php echo $property[3]; ?></p>
-					<?php endif; ?>
+				<div class="four-fifth flex">
+				<?php foreach( $row_headers as $row_header ): ?>
+					<div class="flex-grow" style="max-width:<?php echo $row_width; ?>%">
+						<?php if( empty( $property[ $row_header->slug ] ) ) : ?>
+							<p>&nbsp;</p>
+						<?php elseif( $property[ $row_header->slug ] == 'check' ) : ?>
+							<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/check-green.svg'); ?>"/>
+						<?php elseif( $property[ $row_header->slug ] == 'phone' ) : ?>
+							<img class="accordion-icon" src="<?php echo plugins_url('/accordion-tables/images/phone-green.svg'); ?>"/>
+						<?php else: ?>
+							<p><?php echo $property[ $row_header->slug ]; ?></p>
+						<?php endif; ?>
+					</div>
+				<?php endforeach; ?>
 				</div>
 		</h3>
 		<div class="content-container">
